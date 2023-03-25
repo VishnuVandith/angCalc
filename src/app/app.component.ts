087de46575b5679bf58bad0f0e48b40ef938e847
Vishnu Vandith <vishnuvandith@gmail.com>
2023-03-25 12:56:12 +0530

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  value:any
+  currentValue = ''
+
+  writeToInput(num:any){
+    // alert(num)
+    this.currentValue = this.currentValue + num
+    console.log(this.currentValue);
+    this.value = this.currentValue
+
+  }
+
+  back(){
+    // console.log("Inside Bcak",this.currentValue);
+    this.currentValue = this.currentValue.slice(0,-1)
+    this.value = this.currentValue
+    console.log("after slice",this.currentValue);
+    if(this.value == ''){
+      this.value = '0'
+     this.currentValue = this.value 
+    }
+  }
+
+  clearInput(){
+    this.value = ""
+    this.currentValue = this.value
+  }
+
+  evaluateExp(){
+    this.value = eval(this.currentValue)
+    this.currentValue = this.value
+  }
 }
